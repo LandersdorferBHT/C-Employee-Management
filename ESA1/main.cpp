06fa9500a99dc7c53238e90abb5af85a8767a417
Anton Landersdorfer <s89229@bht-berlin.de>
2024-05-20 12:35:17 +0200

    Initial Commit

---
+++ ESA1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ESA1
+//
+//  Created by Anton Landersdorfer on 20.05.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
